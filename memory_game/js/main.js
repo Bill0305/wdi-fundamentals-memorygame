7fa6542141cffe84bae6966b2161b1448721c518
Bill Bryant <bill0305@gmail.com>
2020-03-22 14:32:44 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,25 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "King";
-let cardFour = "King";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[3];
+//let cardThree = cards[2];
+//let cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+//cardsInPlay.push(cardThree);
+//cardsInPlay.push(cardFour);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+//console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardFour);
+
+if (cardsInPlay.length === 2) {
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
